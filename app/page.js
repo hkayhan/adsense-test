72c3fd3b62f1301fe0cc14e41ff9afaf1d17631e
huseyinkayhan <hkayhann@gmail.com>
2024-07-20 00:44:38 +0300

    test

--- app/page.js
+++ app/page.js
@@ -5,10 +5,14 @@ export default function Home() {
   return (
       <main className={styles.main}>
         hello
-        <ins className="adsbygoogle"
-             style={{ display: 'block' }}
-             data-ad-client="ca-pub-8034519108259358"
-             data-ad-slot="xxxxxxx"></ins>
+          <div style={{backgroundColor:"aliceblue", height:"500px", width:"100%", color:"black"}}>
+asda
+              <ins className="adsbygoogle"
+                   style={{ display: 'block' }}
+                   data-ad-client="ca-pub-8034519108259358"
+                   data-ad-slot="6991267104"></ins>
+          </div>
+
         <script>
           {`(adsbygoogle = window.adsbygoogle || []).push({});`}
         </script>
